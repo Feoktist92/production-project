eeacf4ee5660edc95efd1db81db4cd6ea2d5d018
Igor Feoktistov <feoktist92@gmail.com>
2023-12-21 13:47:43 +0800

    added jest

--- src/shared/lib/classNames.ts
+++ src/shared/lib/classNames.ts
@@ -2,8 +2,8 @@ type Mods = Record<string, boolean | string>;
 
 export function classNames(
     className: string,
-    mods?: Mods,
-    additional?: string[],
+    mods: Mods = {},
+    additional: string[] = [],
 ): string {
     return [
         className,

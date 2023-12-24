e1db3f40998a59df6e8e83f29f462240106f2196
Igor Feoktistov <feoktist92@gmail.com>
2023-12-24 19:07:05 +0800

    add first entity(example), add redux store

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,3 +1,4 @@
+import { Counter } from 'entities/Counter';
 import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
@@ -8,7 +9,11 @@ interface AboutPageProps {
 const AboutPage: FC<AboutPageProps> = () => {
     const { t } = useTranslation('about');
 
-    return <div className={classNames('AboutPage', {}, [])}>{t('О сайте')}</div>;
+    return (
+        <div className={classNames('AboutPage', {}, [])}>
+            {t('О сайте')}
+        </div>
+    );
 };
 
 export default AboutPage;

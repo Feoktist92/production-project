ce9ea2412dc5ab378b30e2155b35731c4b082ce6
Igor Feoktistov <feoktist92@gmail.com>
2023-12-25 22:14:19 +0800

    add feature authByUsername, fixed tests

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,28 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useDispatch } from 'react-redux';
+import { userActions } from 'entities/User';
 import { AppRouter } from './providers/router';
 import { useTheme } from './providers/ThemeProvider';
 
-const App = () => (
-    <div className={classNames('app', {}, [])}>
-        <Suspense fallback="">
-            <Navbar />
-            <div className="content-page">
-                <Sidebar />
-                <AppRouter />
-            </div>
-        </Suspense>
-    </div>
-);
+const App = () => {
+    const dispatch = useDispatch();
 
+    useEffect(() => {
+        dispatch(userActions.initAuthData());
+    }, [dispatch]);
+    return (
+        <div className={classNames('app', {}, [])}>
+            <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
+        </div>
+    );
+};
 export default App;
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,9 +1,10 @@
-import React, { useCallback, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import { Button } from 'shared/ui';
-import { ThemeButton } from 'shared/ui/Button/Button';
+import React, { useCallback, useState } from 'react';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
 import { LoginModal } from 'features/AuthByUsername';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserAuthData, userActions } from 'entities/User';
 import styles from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -13,6 +14,8 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
     const [isAuthModal, setIsAuthModal] = useState(false);
+    const authData = useSelector(getUserAuthData);
+    const dispatch = useDispatch();
 
     const onCloseModal = useCallback(() => {
         setIsAuthModal(false);
@@ -22,16 +25,37 @@ export const Navbar = ({ className }: NavbarProps) => {
         setIsAuthModal(true);
     }, []);
 
+    const onLogout = useCallback(() => {
+        dispatch(userActions.logout());
+    }, [dispatch]);
+
+    if (authData) {
+        return (
+            <div className={classNames(styles.Navbar, {}, [className])}>
+                <Button
+                    theme={ThemeButton.CLEAR_INVERTED}
+                    className={styles.links}
+                    onClick={onLogout}
+                >
+                    {t('Выйти')}
+                </Button>
+            </div>
+        );
+    }
+
     return (
         <div className={classNames(styles.Navbar, {}, [className])}>
             <Button
                 theme={ThemeButton.CLEAR_INVERTED}
-                className={classNames(styles.links, {}, [className])}
+                className={styles.links}
                 onClick={onShowModal}
             >
                 {t('Войти')}
             </Button>
-            <LoginModal isOpen={isAuthModal} onClose={onCloseModal} />
+            <LoginModal
+                isOpen={isAuthModal}
+                onClose={onCloseModal}
+            />
         </div>
     );
 };
